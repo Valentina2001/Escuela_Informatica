b1a7be4a0c0a561187fc48258b7effee04d3af72
Karen Quiceno <ksquiceno3@misena.edu.co>
2019-12-02 16:27:25 -0500

    Se crearon las clases Profesor y ProfesorManager

---
+++ Ejercicio2/Ejercicio2/Profesor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Profesor
+    {
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Domicilio { get; set; }
+    }
+}
---
+++ Ejercicio2/Ejercicio2/ProfesorManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class ProfesorManager
+    {
+        private IDbConnection CreateConnection()
+        {
+            SqlConnection conn = new SqlConnection(@"Server=.\SQLEXPRESS;Database=Ejercicio2;Trusted_Connection=True;");
+            conn.Open();
+            return conn;
+        }
+
+        private void CreateParameter<T>(IDbCommand cmd, string name, T value)
+        {
+            IDbDataParameter prm = cmd.CreateParameter();
+            prm.ParameterName = name;
+            prm.Value = value;
+            cmd.Parameters.Add(prm);
+        }
+
+        public List<Profesor> GetProfesor()
+        {
+            List<Profesor> profesores = new List<Profesor>();
+            using (IDbConnection conn = CreateConnection())
+            {
+                using (IDbCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT dni, nombre, domicilio FROM profesor";
+                    using (IDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            profesores.Add(new Profesor()
+                            {
+                                DNI = Convert.ToInt32(dr["dni"]),
+                                Nombre = dr["nombre"].ToString(),
+                                Domicilio = dr["domicilio"].ToString(),
+                            });
+                        }
+                    }
+                }
+            }
+            return profesores;
+        }
+
+        public void Write(Profesor profesor)
+        {
+            using (IDbConnection conn = CreateConnection())
+            {
+                using (IDbCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO profesor(dni, nombre, domicilio) VALUES(@DNI, @Nombre, @Domicilio)";
+
+                    CreateParameter(cmd, "dni", profesor.DNI);
+                    CreateParameter(cmd, "nombre", profesor.Nombre);
+                    CreateParameter(cmd, "domicilio", profesor.Domicilio);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
