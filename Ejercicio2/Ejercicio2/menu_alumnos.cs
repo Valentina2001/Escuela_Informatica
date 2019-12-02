21da04a3cfe6a51a38015d06b8043ebdbe72e274
Alejandro Muñoz <admunoz261@misena.edu.co>
2019-12-02 18:50:51 -0500

    Se creo el form alumno

--- Ejercicio2/Ejercicio2/menu_alumnos.cs
+++ Ejercicio2/Ejercicio2/menu_alumnos.cs
@@ -10,11 +10,42 @@ using System.Windows.Forms;
 
 namespace Ejercicio2
 {
-    public partial class Form1 : Form
+    public partial class menu_alumnos : Form
     {
-        public Form1()
+        private AlumnoManager _Manager;
+        public menu_alumnos()
         {
             InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            _Manager = new AlumnoManager();
+        }
+
+        public void LimpiarTexto()
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+                if(ctrl is TextBox)
+                {
+                    TextBox text = ctrl as TextBox;
+                    text.Clear();
+                }
+            }
+        }
+
+        private void btnReadAlmno_Click(object sender, EventArgs e)
+        {
+            listAlumno.Items.Clear();
+            List<Alumno> alumnos = _Manager.GetAlumnos();
+            foreach(Alumno alumno in alumnos)
+            {
+                ListViewItem item = listAlumno.Items.Add(alumno.num_matricula.ToString());
+                item.SubItems.Add((alumno.dni).ToString());
+                item.SubItems.Add(alumno.nombre);
+            }
         }
     }
 }
