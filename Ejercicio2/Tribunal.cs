19ae1da2ed4b9799d27edcd181eb5c43eab6de74
Valentina <valen.caldera17@gmail.com>
2019-12-02 21:46:32 -0500

    Arreglando errores

---
+++ Ejercicio2/Tribunal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    public class Tribunal
+    {
+        public int Num { get; set; }
+        public string Lugar_Examen { get; set; }
+        public int Num_Componentes { get; set; }
+    }
+}
